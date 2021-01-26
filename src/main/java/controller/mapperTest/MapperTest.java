6e4291a43fec40dbf64ef3fcbcc7aa9b7f66be96
sunmaolin <Mr.sun727@iCloud.com>
2021-01-27 00:03:57 +0800

    映射器 分页RowBounds、回填主键、自定义主键

---
+++ src/main/java/controller/mapperTest/MapperTest.java
@@ -0,0 +1,76 @@
+package controller.mapperTest;
+
+import dao.RoleMapperTest;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import pojo.Role;
+import util.SqlSessionFactoryUtils;
+
+import java.util.List;
+
+public class MapperTest {
+
+    private static Logger logger = Logger.getLogger(MapperTest.class);
+
+    public static void main(String[] args) {
+        SqlSession sqlSession = null;
+        try{
+            sqlSession = SqlSessionFactoryUtils.openSqlSession();
+//            testRowBounds(sqlSession);
+//            testGenerateKey(sqlSession);
+            testCustomizeKey(sqlSession);
+        }catch (Exception e){
+            e.printStackTrace();
+            //回滚
+            sqlSession.rollback();
+        }finally {
+            if(sqlSession != null){
+                sqlSession.close();
+            }
+        }
+    }
+
+    /**
+     * 测试mybatis自带分页查询,适用于小数据量,大数据量性能不佳
+     * 因为是执行sql后才进行筛选
+     * @param sqlSession
+     */
+    public static void testRowBounds(SqlSession sqlSession){
+        RoleMapperTest roleMapperTest = sqlSession.getMapper(RoleMapperTest.class);
+        //分页限制
+        RowBounds rowBounds = new RowBounds(0,2);
+        List<Role> roles = roleMapperTest.rowBoundsTest(rowBounds);
+        logger.info(roles);
+    }
+
+    /**
+     * 测试主键回填
+     * @param sqlSession
+     */
+    public static void testGenerateKey(SqlSession sqlSession){
+        RoleMapperTest roleMapperTest = sqlSession.getMapper(RoleMapperTest.class);
+        Role role = new Role();
+        role.setRoleName("测试主键回填");
+        role.setRolePhone("15069120633");
+        //返回受影响的行数
+        int affectedRows = roleMapperTest.generateKeyTest(role);
+        logger.info("受影响的行数:"+affectedRows);
+        logger.info(role);
+    }
+
+    /**
+     * 测试插入自定义主键 规则:主键最大值+3
+     * @param sqlSession
+     */
+    public static void testCustomizeKey(SqlSession sqlSession){
+        RoleMapperTest roleMapperTest = sqlSession.getMapper(RoleMapperTest.class);
+        Role role = new Role();
+        role.setRoleName("测试自定义主键");
+        int affectedRows = roleMapperTest.customizeKeyTest(role);
+        logger.info(affectedRows);
+        logger.info(role);
+    }
+
+
+}
--- src/main/java/dao/RoleMapperTest.java
+++ src/main/java/dao/RoleMapperTest.java
@@ -1,8 +1,10 @@
 package dao;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
 import pojo.Role;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -39,5 +41,27 @@ public interface RoleMapperTest {
      * @return
      */
     Role parameterMixTest(@Param("role")Role role,@Param("limitMap")Map<String,Integer> limitMap);
+
+    /**
+     * 测试mybatis自带rowBounds分页查询,用于小数据量
+     * 因为mybatis执行sql查询后从结果集中进行筛选,大数据量性能并不佳
+     * @param rowBounds mybatis自带分页查询 默认是0开始 最大条数为Integer的最大数
+     * @return
+     */
+    List<Role> rowBoundsTest(RowBounds rowBounds);
+
+    /**
+     * 测试主键回填
+     * @param role
+     * @return
+     */
+    int generateKeyTest(Role role);
+
+    /**
+     * 测试插入自定义主键,在最大主键+3插入
+     * @param role
+     * @return
+     */
+    int customizeKeyTest(Role role);
 
 }
